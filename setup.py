ba866611aefb3f846f1d75e110975c27ff8ee383
vyathakavilocana <prathammc@gmail.com>
2020-11-21 18:12:46 -0500

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The first AI club project for 2020 pertains to the DS/ML code for the [D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[. It pertains to the DS/ML code for the [A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C at NC State project for 2020. It pertains to the DS/ML code used to create an application regarding analyzing news sources for the 2020 election starting out with a recommendation engine and branching into other aspects. ',
+    author='Pratham Chhabria',
+    license='MIT',
+)
